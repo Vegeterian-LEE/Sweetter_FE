12b8eba24629c4c5d899c3ab2ac72ab5e9fe464a
hdlee0619 <dlgusehd1211@naver.com>
2023-03-03 23:09:29 +0900

    Design: SearchBar.jsx 추가

--- src/components/Layout/SearchBar.jsx
+++ src/components/Layout/SearchBar.jsx
@@ -1,12 +1,52 @@
 import React from "react";
 import styled from "styled-components";
 
+import { FiSearch } from "react-icons/fi";
+
 const SearchBar = () => {
-  return <SearchBarContainer>SearchBar</SearchBarContainer>;
+  return (
+    <>
+      <SearchBarContainer>
+        <SearchForm>
+          <FiSearch size={25} />
+          <SearchSweetterInput placeholder="serch"></SearchSweetterInput>
+        </SearchForm>
+        <FollowListWrapper>Follow List</FollowListWrapper>
+      </SearchBarContainer>
+    </>
+  );
 };
 
 const SearchBarContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   width: 28vw;
+  padding: 17px;
+`;
+
+const SearchSweetterInput = styled.input`
+  padding-left: 17px;
+  border-radius: 20px;
+  height: 40px;
+  background-color: #eff3f4;
+`;
+
+const SearchForm = styled.form`
+  display: flex;
+  align-items: center;
+  border-radius: 25px;
+  width: 85%;
+  padding-left: 20px;
+  background-color: #eff3f4;
+`;
+
+const FollowListWrapper = styled.div`
+  width: 85%;
+  height: 50%;
+  margin-top: 20px;
+  border-radius: 25px;
+  background-color: #eff3f4;
 `;
 
 export default SearchBar;
