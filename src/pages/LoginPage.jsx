df3226f1715aa1e2480f0aeb90290c13cec48f03
juninkorea95 <juninkorea95@gmail.com>
2023-03-04 16:18:39 +0900

    feat| login 회원가입 로직 구현(중)

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, useState, useRef } from "react";
 import styled from "styled-components";
 
 import TwitterLogo from "../assets/TwitterLogo.jpg";
@@ -9,10 +9,93 @@ import ModalBox from "../components/elements/modal/ModalBox";
 
 import useOutSideClick from "../components/hooks/useOutsideClick";
 
-import { useState, useRef } from "react";
 import { FaTwitter } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+
+import axios from "axios";
 
 const LoginPage = () => {
+  const [userId, setUserId] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // 오류 메세지
+  const [usernameMessage, setUsernameMessage] = useState("");
+  const [passwordMessage, setPasswordMessage] = useState("");
+
+  // 유효성 검사 둘다 true 일시 버튼 클릭 가능
+  const [isUsername, setIsUsername] = useState(false);
+  const [isPassword, setIsPassword] = useState(false);
+
+  const navigate = useNavigate();
+
+  //userId input change
+
+  const onChangeUserId = (e) => {
+    setUserId(e.target.value);
+  };
+
+  // username input change
+  const onChangeUsername = (e) => {
+    setUsername(e.target.value);
+
+    const idRegex = /^(?=.*?[0-9])(?=.*?[a-z]).{4,10}$/;
+
+    if (!idRegex.test(e.target.value)) {
+      setUsernameMessage(
+        "영어 소문자, 숫자 각각 1개 이상, 총 4자리-10자리여야 합니다."
+      );
+      setIsUsername(false);
+    } else {
+      setUsernameMessage("올바른 id 형식입니다");
+      setIsUsername(true);
+    }
+  };
+
+  // email input change
+
+  const onChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  // pw input change
+  const onChangePassword = (e) => {
+    setPassword(e.target.value);
+    const idRegex = /^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,15}$/;
+
+    if (!idRegex.test(e.target.value)) {
+      setPasswordMessage(
+        "패스워드는 8자리-15자리로, 영어 소문자, 숫자, 특수문자가 각각 1개 이상이여야 합니다."
+      );
+      setIsPassword(false);
+    } else {
+      setPasswordMessage("올바른 패스워드 형식입니다.");
+      setIsPassword(true);
+    }
+  };
+
+  // 회원가입
+  const joinHandler = async () => {
+    if (isUsername === true && isPassword === true) {
+      try {
+        const newUser = {
+          userId,
+          username,
+          email,
+          password,
+        };
+        await axios.post("", newUser);
+        alert("회원가입 성공 !!");
+        navigate("/");
+      } catch (error) {
+        alert(error.response.data.message);
+      }
+    } else {
+      alert("규정에 맞는 아이디와 비밀번호를 입력해 주세요!");
+    }
+  };
+
   // 로그인 모달 관리
   const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
 
@@ -84,11 +167,33 @@ const LoginPage = () => {
             <div ref={signupModalRef}>
               <StMiniFaTwitter />
               <StLogin>트위터 회원가입</StLogin>
-              <StInput type="text" placeholder="닉네임" />
-              <StInput type="text" placeholder="유저 네임" />
-              <StInput type="text" placeholder="이메일" />
-              <StInput type="text" placeholder="비밀번호" />
-              <Button wh="l" width="350px">
+              <StInput
+                type="text"
+                placeholder="User ID"
+                value={userId}
+                onChange={onChangeUserId}
+              />
+              <StInput
+                type="text"
+                placeholder="이메일"
+                value={email}
+                onChange={onChangeEmail}
+              />
+              <StInput
+                type="text"
+                placeholder="User Name"
+                value={username}
+                onChange={onChangeUsername}
+              />
+              <StMessage>{usernameMessage}</StMessage>
+              <StInput
+                type="text"
+                placeholder="비밀번호"
+                value={password}
+                onChange={onChangePassword}
+              />
+              <StMessage>{passwordMessage}</StMessage>
+              <Button wh="l" width="350px" onClick={joinHandler}>
                 가입 완료
               </Button>
             </div>
@@ -187,6 +292,16 @@ export const StInput = styled.input`
   &:hover::placeholder {
     color: #4da0eb;
   }
+`;
+
+const StMessageBox = styled.div`
+  position: relative;
+  margin-top: 1rem;
+`;
+
+const StMessage = styled.p`
+  font-size: 7px;
+  color: #0c85d0;
 `;
 
 export const StToSignUp = styled.h3`
