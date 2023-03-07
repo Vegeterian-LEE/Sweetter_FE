b70e87299f57eba79e928dd332894adb3e27447d
hdlee0619 <dlgusehd1211@naver.com>
2023-03-08 01:51:21 +0900

    Fix| import 경로 수정

--- src/pages/Main/MainPage.jsx
+++ src/pages/Main/MainPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Layout from "../../components/Layout/Layout";
+import Layout from "../../components/layout/Layout";
 import PostLayout from "./_base/PostLayout";
 
 const MainPage = () => {
