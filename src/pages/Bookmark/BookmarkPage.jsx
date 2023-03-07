b48eeb18ad9b73c1f9c6a5ad424d2a5205efec16
juninkorea95 <juninkorea95@gmail.com>
2023-03-08 01:35:57 +0900

    Fix | 경로수정

--- src/pages/Bookmark/BookmarkPage.jsx
+++ src/pages/Bookmark/BookmarkPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Layout from "../../components/layout/Layout";
+import Layout from "../../components/Layout/Layout";
 
 import BookmarkLayout from "./_base/BookmarkLayout";
 
