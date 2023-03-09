160305868b7742f19fefa4fbd0338d13eb4da394
hdlee0619 <dlgusehd1211@naver.com>
2023-03-09 15:33:09 +0900

    Feat | Scroll To Top 기능 추가

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,22 +1,19 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import MainPage from "../pages/Main/MainPage";
 import LoginPage from "../pages/Login/LoginPage";
 import DetailPage from "../pages/Detail/DetailPage";
 import BookmarkPage from "../pages/Bookmark/BookmarkPage";
 import ProfilePage from "../pages/Profile/ProfilePage";
-import ModalComment from "../components/modals/ModalComment";
 
 const Router = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/detail/:id" element={<DetailPage />} />
-        <Route path="/bookmark" element={<BookmarkPage />} />
-        <Route path="/profile/:id" element={<ProfilePage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/detail/:id" element={<DetailPage />} />
+      <Route path="/bookmark" element={<BookmarkPage />} />
+      <Route path="/profile/:id" element={<ProfilePage />} />
+    </Routes>
   );
 };
 
